ae6060ca06300de191c94ef08b5872a66c564868
yogeshkatari56-yk <yogeshkatari56@gmail.com>
2026-03-02 19:55:54 +0530

    v32

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://sdp2026backend.onrender.com/api' });
 
 // Add token to headers for protected routes
 API.interceptors.request.use((req) => {
